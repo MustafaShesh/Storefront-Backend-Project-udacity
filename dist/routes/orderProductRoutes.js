dba72dde906b9b12d3efe05691ca7b73441769c4
MustafaShesh <mostafagamal9591@yahoo.com>
2022-12-14 18:55:58 +0200

    remove .env and dist directory

--- dist/routes/orderProductRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var orderProductHandler_1 = require("../handlers/orderProductHandler");
-var orderProductsRoutes = function (app) {
-    // add product
-    app.post('/orders/:id/products', orderProductHandler_1.addProduct);
-};
-exports.default = orderProductsRoutes;
